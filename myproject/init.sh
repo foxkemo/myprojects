c2ae25a9957f57960c55744928b31eb8f2ec6ff3
unomi <taokunkun1@icloud.com>
2025-03-23 23:44:30 +0800

    first_commit

---
+++ myproject/init.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo "this script will delete the bin folder that is already exist , continue(y)?"
+read choice
+if [ "$choice"="y" ]
+then
+if [ -d "bin" ]
+then
+rm -rf bin
+fi
+if [ -f "source.txt" ]
+then
+rm source.txt
+fi
+mkdir bin
+find src -name "*.java" > source.txt
+javac -d bin @source.txt
+jar --create --file myjar.jar --main-class com.mypack.Main  -C bin .
+rm source.txt
+else
+exit
+fi
---
+++ myproject/src/com/mypack/Main.java
@@ -0,0 +1,6 @@
+import java.util.Scanner;
+public class Main {
+  public static void main(){
+    System.out.println("hello");
+  }
+}
