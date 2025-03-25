0c1c0cb314ae064fdff06354184753ba747221b0
unomi <taokunkun1@icloud.com>
2025-03-25 08:21:23 +0800

    meow

--- myproject/src/com/mypack/Main.java
+++ myproject/src/com/mypack/Main.java
@@ -1,7 +1,26 @@
 package com.mypack;
 import java.util.Scanner;
+import com.mypack.Test;
 public class Main {
   public static void main(String[] args){
-    System.out.println("hello");
+    System.out.println("hello ,input your choice(1,2,3) to run test(1,2,3) method");
+    int choice;
+    Scanner scan=new Scanner(System.in);
+    choice=scan.nextInt();
+    switch (choice){
+      case 1:
+      Test.test1();
+      break;
+      case 2:
+      Test.test2();
+      break;
+      case 3:
+      Test.test3();
+      break;
+      default:
+      System.out.println("invalid input");
+    }
+
+
   }
 }
---
+++ myproject/src/com/mypack/Test.java
@@ -0,0 +1,14 @@
+package com.mypack;
+import java.util.Scanner;
+public class Test{
+    static void test1 (){
+      System.out.println("test1");
+    }
+    static void test2 (){
+      System.out.println("test2");
+    }
+    static void test3 (){
+      System.out.println("test3");
+    }
+
+}
