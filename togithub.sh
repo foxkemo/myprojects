5d1cf1922c75da068a4c32138ca0a6c54f100904
unomi <taokunkun1@icloud.com>
2025-04-05 15:10:17 +0800

    meow

--- togithub.sh
+++ togithub.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-git add .
+hexo g -d
+cd ~/Downloads/foxkemo.github.io/
+git rm -r .
+cp -r ../myprojects/myblog/public/* .
+git add *
+git commit -m "update"
+git push
+cd  ../myprojects                                  
+git add *
 git commit -m meow
 git push
