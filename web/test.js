de37b4c2bdd18e6036b0c2097091a960d40d14d4
Unomi <168223017+foxkemo@users.noreply.github.com>
2025-07-10 19:52:16 +0800

    meow

---
+++ web/test.js
@@ -0,0 +1,12 @@
+let userMeta={
+    name:"",
+    email:"",
+    password:"",
+    birthday:"",
+}
+alert ("Please login or register");
+let userInfo=new Map();
+userInfo.set("remainingDownloadTimes",0);
+userInfo.set("totalDownloadTimes",0);
+
+
